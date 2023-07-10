bb5b30aa5245ebb6164674e4c7dc3e86f73d6c53
Sophian Laroy <sophian.laroy@gmail.com>
2023-07-10 15:32:27 +0200

    Create project

---
+++ app/src/main/java/com/laroy/adscientiamtest/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.laroy.adscientiamtest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.laroy.adscientiamtest.ui.theme.AdScientiamTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdScientiamTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AdScientiamTestTheme {
+        Greeting("Android")
+    }
+}
